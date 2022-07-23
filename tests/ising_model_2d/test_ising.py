3a1755419d0c89e8edaa278d866948e254cf77d8
Solomon Owerre <alaowerre@gmail.com>
2022-07-23 19:30:45 -0400

    rearranged folder structure

--- tests/ising_model_2d/test_ising.py
+++ tests/ising_model_2d/test_ising.py
@@ -5,7 +5,7 @@
 #####################################
 
 import sys
-base_path = ''
+base_path = '/Users/sowerre/Documents/python/'
 sys.path.append(base_path + 'monte-carlo/monte_carlo/ising_model_2d/src/')
 import Ising_model_2d as ising
 
